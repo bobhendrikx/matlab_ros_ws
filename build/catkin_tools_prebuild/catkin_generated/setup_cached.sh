204fb03fe8553ca8b3bab25299c575979fa6ede2
Bob Hendrikx <hendrikx.bob@gmail.com>
2018-01-25 13:46:08 +0100

    repo for testing Matlab-ROS realtime stuff

---
+++ build/catkin_tools_prebuild/catkin_generated/setup_cached.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/bob/matlab_ros_ws/devel/.private/catkin_tools_prebuild:/home/bob/ros/kinetic/system/devel:/opt/ros/kinetic:/home/bob/ros/kinetic/dev/devel"
+export LD_LIBRARY_PATH="/home/bob/ros/kinetic/system/devel/lib:/opt/ros/kinetic/lib:/opt/ros/kinetic/lib/x86_64-linux-gnu:/usr/local/cuda-8.0/lib64:/home/bob/torch/install/lib:/home/bob/ros/kinetic/dev/devel/lib:/home/bob/torch/install/lib:"
+export PATH="/home/bob/ros/kinetic/system/devel/bin:/opt/ros/kinetic/bin:/usr/local/cuda-8.0/bin:/home/bob/torch/install/bin:/home/bob/.tue/bin:/usr/lib/ccache:/home/bob/.tue/installer/targets/sergio-user/scripts:/home/bob/.tue/installer/targets/amigo-user/scripts:/home/bob/torch/install/bin:/home/bob/bin:/home/bob/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin"
+export PKG_CONFIG_PATH="/home/bob/ros/kinetic/system/devel/lib/pkgconfig:/opt/ros/kinetic/lib/pkgconfig:/opt/ros/kinetic/lib/x86_64-linux-gnu/pkgconfig:/home/bob/ros/kinetic/dev/devel/lib/pkgconfig"
+export PWD="/home/bob/matlab_ros_ws/build/catkin_tools_prebuild"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/bob/matlab_ros_ws/devel/.private/catkin_tools_prebuild/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH="/home/bob/matlab_ros_ws/build/catkin_tools_prebuild:$ROS_PACKAGE_PATH"
